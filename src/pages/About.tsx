5b191267c3a11229890e45195101bd400e0696e7
gubmax <gubyshev@gmail.com>
2022-09-04 19:57:23 +0300

    feat(client): add vanilla-extract

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,9 @@
 import { memo } from 'react'
 
+import * as s from './About.css'
+
 function About() {
-  return <h1>About</h1>
+  return <h1 className={s.title}>About</h1>
 }
 
 export default memo(About)
