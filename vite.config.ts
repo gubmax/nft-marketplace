0c1933a6bd10cade659aa7f23fd792aab9dbd144
gubmax <gubyshev@gmail.com>
2022-09-20 21:56:06 +0300

    style: format config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
-import react from '@vitejs/plugin-react'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 /**
  * @link https://vitejs.dev/config/
  */
 export default defineConfig(({ ssrBuild }) => {
-  const plugins = [
-    tsconfigPaths(),
-    react(),
-    vanillaExtractPlugin(),
-    splitVendorChunkPlugin(),
-  ]
+  const plugins = [tsconfigPaths(), react(), vanillaExtractPlugin(), splitVendorChunkPlugin()]
 
   return ssrBuild
     ? {
