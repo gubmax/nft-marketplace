6e1dddfeb73e6d89b9587cbf36adf615e4e2f1aa
gubmax <gubyshev@gmail.com>
2022-10-04 19:42:47 +0300

    refactor: update external list

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,32 +1,26 @@
+import { builtinModules } from 'node:module'
+
 import { build } from 'esbuild'
 import pc from 'picocolors'
 
-import packageJson from '../package.json'
+import pkg from '../package.json'
 
 process.env.NODE_ENV = 'production'
 
-const OUTFILE = 'index.js'
-
-function getExternal(
-  packages: Record<string, boolean | string | string[] | Record<string, unknown>>,
-): string[] {
-  return Object.keys(packages).reduce<string[]>((acc, key) => {
-    if (key.includes('ependencies')) {
-      acc.push(...Object.keys(packages[key]))
-    }
-
-    return acc
-  }, [])
-}
-
 console.log(`${pc.cyan('esbuild')} ${pc.green('building server for production...')}`)
+
+const OUTFILE = 'index.js'
 
 build({
   platform: 'node',
   target: 'node16',
   bundle: true,
   sourcemap: false,
-  external: getExternal(packageJson),
+  external: [
+    ...builtinModules,
+    ...Object.keys(pkg.dependencies),
+    ...Object.keys(pkg.devDependencies),
+  ],
   entryPoints: ['./src/server/main.ts'],
   outfile: `./dist/server/${OUTFILE}`,
   format: 'esm',
