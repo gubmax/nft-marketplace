999c82c32e159e708f881cb491140271fc1f21d8
gubmax <gubyshev@gmail.com>
2022-10-27 22:20:48 +0300

    enh(client): not found layout

--- src/client/modules/notFound/components/NotFound/NotFound.css.ts
+++ src/client/modules/notFound/components/NotFound/NotFound.css.ts
@@ -1,20 +1,39 @@
 import { style } from '@vanilla-extract/css'
 
 import { dt } from 'client/common/styles/designTokens'
-import { pxToRem } from 'client/common/styles/helpers'
 
 export const wrapper = style({
+  height: '100vh',
   padding: dt.sys.space.s3,
   textAlign: 'center',
   overflow: 'hidden',
 })
 
-export const title = style({
-  WebkitBackgroundClip: 'text',
-  WebkitTextFillColor: 'transparent',
-  backgroundClip: 'text',
-  backgroundImage: dt.ref.gradients.primary,
-  display: 'block',
-  fontSize: pxToRem(168),
-  fontWeight: 700,
+export const bgText = style({
+  position: 'absolute',
+  top: '50%',
+  fontSize: '45vw',
+  fontWeight: 900,
+  color: dt.ref.palette.borderLight,
+  zIndex: -1,
+  transform: 'rotate(4deg) translateY(-50%)',
+
+  '@media': {
+    [dt.sys.media.maxWidth.tablet]: {
+      position: 'unset',
+      transform: 'rotate(4deg)',
+    },
+  },
 })
+
+export const title = style([
+  dt.comp.typography.h1,
+  {
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    backgroundClip: 'text',
+    backgroundImage: dt.ref.gradients.primary,
+    display: 'block',
+    fontWeight: 700,
+  },
+])
--- src/client/modules/notFound/components/NotFound/NotFound.tsx
+++ src/client/modules/notFound/components/NotFound/NotFound.tsx
@@ -1,27 +1,24 @@
 import { memo } from 'react'
 
+import { Button } from 'client/common/components/inputs/buttons/Button'
+import { PageRoutes } from 'client/common/constants'
 import { cn } from 'client/common/helpers/classNames'
+import { useLink } from 'client/common/hooks/useLink'
 import * as a from 'client/common/styles/atomic.css'
 import { dt } from 'client/common/styles/designTokens'
-import { StyledProps } from 'client/common/typings'
 import * as s from './NotFound.css'
 
-function NotFound({ className, style }: StyledProps) {
+function NotFound() {
+  const navigateToHome = useLink(PageRoutes.HOME)
+
   return (
-    <section
-      className={cn(
-        s.wrapper,
-        a.flex,
-        a.flexCol,
-        a.mAuto,
-        a.justifyCenter,
-        a.itemsCenter,
-        className,
-      )}
-      style={style}
-    >
-      <span className={cn(s.title, a.mb3)}>404</span>
-      <h1 className={dt.comp.typography.h1}>Page Not Found</h1>
+    <section className={cn(s.wrapper, a.flex, a.flexCol, a.justifyCenter, a.itemsCenter)}>
+      <span className={s.bgText}>404</span>
+      <span className={cn(s.title, a.mb2)}>Ooops...</span>
+      <h1 className={cn(dt.comp.typography.h1, a.mb5)}>Page Not Found</h1>
+      <Button as="a" variant="contained" href={PageRoutes.HOME} onClick={navigateToHome}>
+        Go home
+      </Button>
     </section>
   )
 }
