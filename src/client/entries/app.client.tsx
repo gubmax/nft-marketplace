fd3fc0c45fd22d5066fd7cc8e1d108c9f2013f9f
gubmax <gubyshev@gmail.com>
2022-11-25 19:13:21 +0300

    enh(client): add web-vitals report

--- src/client/entries/app.client.tsx
+++ src/client/entries/app.client.tsx
@@ -1,6 +1,7 @@
 import { hydrateRoot } from 'react-dom/client'
 import { createBrowserRouter } from 'react-router-dom'
 
+import { reportWebVitals } from 'client/common/utils/reportWebVitals'
 import { App } from 'client/modules/layout/app'
 import { ROUTES } from 'client/routes'
 import 'client/common/styles/global.css'
@@ -8,3 +9,7 @@ import 'client/common/styles/global.css'
 const router = createBrowserRouter(ROUTES)
 
 hydrateRoot(document.getElementById('app') ?? document.body, <App router={router} />)
+
+if (process.env.NODE_ENV === 'production') {
+  reportWebVitals(console.log)
+}
