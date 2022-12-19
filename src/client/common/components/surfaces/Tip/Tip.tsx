9779447fe7df3d5834a40534ec72c36194460ebc
gubmax <gubyshev@gmail.com>
2022-12-19 22:15:16 +0300

    enh(client): update TIP layout

--- src/client/common/components/surfaces/Tip/Tip.tsx
+++ src/client/common/components/surfaces/Tip/Tip.tsx
@@ -1,4 +1,3 @@
-import { TipIcon } from 'client/common/components/icons'
 import { cn } from 'client/common/helpers/classNames'
 import { ChildrenProp, StyledProps } from 'client/common/typings'
 import s from './Tip.module.css'
@@ -7,8 +6,8 @@ type TipProps = ChildrenProp & StyledProps
 
 function Tip({ className, style, children }: TipProps) {
   return (
-    <div className={cn(s.wrapper, 'flex items-center', className)} style={style}>
-      <TipIcon className="self-start flex-shrink-0 mr-4" variant="primary" />
+    <div className={cn(s.wrapper, className)} style={style}>
+      <span className={cn(s.title, 'mb-4')}>TIP</span>
       <div>{children}</div>
     </div>
   )
