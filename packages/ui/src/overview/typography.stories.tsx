b49485931c815c528aec3bde2d9b097ab03795b5
gubmax <gubyshev@gmail.com>
2023-04-23 20:47:53 +0200

    enh: add font sizes

--- packages/ui/src/overview/typography.stories.tsx
+++ packages/ui/src/overview/typography.stories.tsx
@@ -9,11 +9,21 @@ export default meta
 export const Typography: StoryObj = {
 	render: () => (
 		<div className="flex flex-col gap-4">
-			<span className="text-headline">Headline — Semibold</span>
-			<span className="text-subhead">Subhead — Semibold</span>
-			<span className="text-body">Body — Regular</span>
-			<span className="text-caption">Caption — Regular</span>
-			<span className="text-detail">Detail — Bold</span>
+			<span className="text-display-lg">Display Large</span>
+			<span className="text-display-md">Display Medium</span>
+			<span className="text-display-sm">Display Small</span>
+			<span className="text-headline-lg">Headline Large</span>
+			<span className="text-headline-md">Headline Medium</span>
+			<span className="text-headline-sm">Headline Small</span>
+			<span className="text-title-lg">Title Large</span>
+			<span className="text-title-md">Title Medium</span>
+			<span className="text-title-sm">Title Small</span>
+			<span className="text-label-lg">Label Large</span>
+			<span className="text-label-md">Label Medium</span>
+			<span className="text-label-sm">Label Small</span>
+			<span className="text-body-lg">Body Large</span>
+			<span className="text-body-md">Body Medium</span>
+			<span className="text-body-sm">Body Small</span>
 		</div>
 	),
 }
--- packages/ui/uno.config.ts
+++ packages/ui/uno.config.ts
@@ -4,6 +4,23 @@ import { defineConfig } from 'unocss'
 export default defineConfig({
 	presets: [presetMini({ variablePrefix: 'ui-' })],
 	theme: {
+		fontSize: {
+			'display-lg': ['57px', '64px'],
+			'display-md': ['45px', '52px'],
+			'display-sm': ['35px', '44px'],
+			'headline-lg': ['32px', '40px'],
+			'headline-md': ['28px', '36px'],
+			'headline-sm': ['24px', '32px'],
+			'title-lg': ['22px', '28px'],
+			'title-md': ['16px', '24px'],
+			'title-sm': ['14px', '20px'],
+			'label-lg': ['14px', '20px'],
+			'label-md': ['12px', '16px'],
+			'label-sm': ['11px', '16px'],
+			'body-lg': ['16px', '24px'],
+			'body-md': ['14px', '20px'],
+			'body-sm': ['12px', '16px'],
+		},
 		colors: {
 			// System
 			inherit: 'inherit',
